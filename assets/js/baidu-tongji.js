52a8ea88c258f16b38f4c1011e0ffef66b47dcb2
mahoo12138 <mahoo12138@qq.com>
2020-08-17 13:56:54 +0800

    Deployed 626451a with MkDocs version: 1.1.2

---
+++ assets/js/baidu-tongji.js
@@ -0,0 +1,23 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?856f9eaf39cdeb5f0a0863a93279decb";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+
+</script>
